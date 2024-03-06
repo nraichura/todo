baab71768a3c4d73f19c7cae5cbb12c402ca4f96
Nitin Raichura <nitin.raichura@onelogic.de>
2024-03-06 23:58:24 +0100

    formatting

--- src/main/java/com/simplesystem/todo/service/UpdatePastDueStatusScheduler.java
+++ src/main/java/com/simplesystem/todo/service/UpdatePastDueStatusScheduler.java
@@ -4,23 +4,31 @@ import com.simplesystem.todo.entity.Item;
 import com.simplesystem.todo.entity.ItemStatus;
 import com.simplesystem.todo.repository.ItemRepository;
 import com.simplesystem.todo.utility.TransactionHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
 @Component
-public record UpdatePastDueStatusScheduler(ItemRepository itemRepository, TransactionHandler transactionHandler) {
+@RequiredArgsConstructor
+public class UpdatePastDueStatusScheduler {
 
-    @Scheduled(fixedDelayString = "${updatePastDueStatus.scheduler.fixedDelay}")
-    public void updatePastDueStatus() {
-        transactionHandler.runInTransaction(() -> itemRepository.findAll().stream()
-                .filter(this::isItemAlreadyDue)
-                .peek(item -> item.setStatus(ItemStatus.PAST_DUE))
-                .toList());
-    }
+  private final ItemRepository itemRepository;
+  private final TransactionHandler transactionHandler;
 
-    private boolean isItemAlreadyDue(Item item) {
-        return item.getDueAt().isBefore(Instant.now());
-    }
+  @Scheduled(fixedDelayString = "${updatePastDueStatus.scheduler.fixedDelay}")
+  public void updatePastDueStatus() {
+    transactionHandler.runInTransaction(
+        () -> {
+          itemRepository.findAll().stream()
+              .filter(this::isItemAlreadyDue)
+              .forEach(item -> item.setStatus(ItemStatus.PAST_DUE));
+          return null;
+        });
+  }
+
+  private boolean isItemAlreadyDue(Item item) {
+    return item.getDueAt().isBefore(Instant.now());
+  }
 }
