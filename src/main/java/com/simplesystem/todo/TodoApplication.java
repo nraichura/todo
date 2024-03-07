23b46d23d1c9f70f0b21b4d863a0d1e1385c3304
Nitin Raichura <nitin.raichura@onelogic.de>
2024-03-07 08:53:52 +0100

    Show proper date format while serialization.

--- src/main/java/com/simplesystem/todo/TodoApplication.java
+++ src/main/java/com/simplesystem/todo/TodoApplication.java
@@ -1,6 +1,7 @@
 package com.simplesystem.todo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +20,7 @@ public class TodoApplication {
   ObjectMapper objectMapper() {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     return objectMapper;
   }
 }
